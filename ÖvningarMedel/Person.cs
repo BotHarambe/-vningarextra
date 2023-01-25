cfb18214847de3a5f386d4035f0ee687c7a196a5
Samurai <kylett63@gmail.com>
2023-01-25 14:49:05 +0100

    Add project files.

---
+++ ÖvningarMedel/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ÖvningarMedel
+{
+    class Person
+    {       public string Name { get; set; }
+            public int Age { get; set; }
+
+            public Person(string name, int age)
+            {
+                Name = name;
+                Age = age;
+            }
+        }
+
+        class Student : Person
+        {
+            public int StudentId { get; set; }
+
+            public Student(string name, int age, int studentId) : base(name, age)
+            {
+                StudentId = studentId;
+            }
+        }
+
+        class Staff : Person
+        {
+            public string StaffId { get; set; }
+
+            public Staff(string name, int age, string staffId) : base(name, age)
+            {
+                StaffId = staffId;
+            }
+        }
+
+    }
+
---
+++ ÖvningarMedel/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ÖvningarMedel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            while (x == 0)
+            {
+                var staffList = new List<Staff>();
+                var studentList = new List<Student>();
+
+                int numStaff = 2;
+                int numStudent = 5;
+
+                for (int i = 0; i < numStaff; i++)
+                {
+                    staffList.Add(new Staff("Staff" + (i + 1), 30, "S" + (i + 1)));
+                    Console.WriteLine(staffList[i].Name);
+                }
+
+                for (int i = 0; i < numStudent; i++)
+                {
+                    studentList.Add(new Student("Student" + (i + 1), 20, i + 1));
+                    Console.WriteLine(studentList[i].Name);
+                }
+                x = 1;
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
